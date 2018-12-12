7e44617d4579ce14f7e45aa602f6282efc07ee08
GregHilston <Gregory.Hilston@gmail.com>
2018-12-12 12:55:18 -0700

    Finished day 2 part 2

--- day_2/part_2/README.md
+++ day_2/part_2/README.md
@@ -20,3 +20,16 @@ What letters are common between the two correct box IDs? (In the example above, 
 
 ## My Comments
 
+This problem suggests that we'd use [Hamming distance](https://en.wikipedia.org/wiki/Hamming_distance) to compare two strings. 
+
+My first inclination was for every row in the input, check every row in the input below and calculate the hamming distance. This seemed like a brute force method and instead I'm going to use a trie to minimize the number of checks needed. Visualization (created [here](https://www.cs.usfca.edu/~galles/visualization/Trie.html))
+
+![trie example](trie_example.png)
+
+We check every fork in the Trie and calculate the hamming distance there.
+
+Just found out about [setdefault](https://docs.python.org/3/library/stdtypes.html), game changing.
+
+So we actually build the trie and then walk the common children, checking only those paths for a hamming distance of 1. 
+
+**Please note, this solution does not work if the two words in common both start with different letters** This is because the trie starts with the root node and forks for different letters and we only search common starting letters words. We could fix this, but I chose not too.
--- day_2/part_2/solution.py
+++ day_2/part_2/solution.py
@@ -1,37 +1,76 @@
+import sys
 from datetime import datetime # to time how long execution took
 
 startTime = datetime.now()
 
-input_file_name = "input.txt"
+input_file_name = "../input.txt"
+_end = '*'
 
-with open(input_file_name) as handle:
-    two_char_count = 0
-    three_char_count = 0
+def hamming_distance(str1, str2):
+    """From https://stackoverflow.com/questions/31007054/hamming-distance-between-two-binary-strings-not-working"""
+    diff_count = 0
 
-    for line in handle:
-        d = {}
-        already_found_two_char = False
-        already_found_three_char = False
+    for ch1, ch2 in zip(str1, str2):
+        if ch1 != ch2:
+            diff_count += 1
+    
+    return diff_count
 
-        for char in line:
-            if char in d:
-                d[char] += 1
-            else:
-                d[char] = 1
+def find_common_characters(str1, str2):
+    common_characters = []
+
+    for ch1, ch2 in zip(str1, str2):
+        if ch1 == ch2:
+            common_characters.append(ch1)
+
+    return ''.join(str(x) for x in common_characters)
+
+def look_for_other_word_one_hamming_distance_away(root, base_word, built_word):
+    # print(f"root {root}")
+    # print(f"base_word {base_word}")
+
+    if isinstance(root, dict):
+        for letter, d in root.items():
+            # print(f"\tbuilt_word {built_word}")
+            # print(f"\tletter {letter}")
+            # print(f"\td {d}")
+
+            look_for_other_word_one_hamming_distance_away(d, base_word, built_word + letter)
+    else:
+        built_word = built_word.strip(_end)
+        # print("\tdone")
+        # print(f"\tbuilt_word {built_word}")
+
+        if hamming_distance(base_word, built_word) == 1:
+            print(f"found two words one hamming distance away: {base_word} and {built_word}")
+            
+            common_characters = find_common_characters(base_word, built_word)
+            print(f"common characters between two strings with hamming distance of 1 {common_characters}")
+
+            sys.exit(0)
+
+def construct_trie(words):
+    root = dict()
+
+    for word in words:
+        current_dict = root
         
-        print(d)
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict[_end] = _end
 
-        for key, value in d.items():
-            # print(f"value = {value}, already_found_two_char = {already_found_two_char} and already_found_three_char = {already_found_three_char}")
-            if value == 2 and already_found_two_char == False:
-                two_char_count += 1
-                already_found_two_char = True
-                # print(f"\tfound two pair {key}")
-            elif value == 3 and already_found_three_char == False:
-                three_char_count += 1
-                already_found_three_char = True
-                # print(f"\tfound three pair {key}")
-        #break
+        # print("start")
+        look_for_other_word_one_hamming_distance_away(root, word, "")
+    return root
 
-print(f"checksum = {two_char_count * three_char_count}")
+with open(input_file_name) as file:
+    ids = []
+    
+    for line in file:
+        ids.append(line.strip('\n'))
+
+    trie = construct_trie(ids)
+
+    # print(trie)
+
 print(f"execution took {datetime.now() - startTime}")
